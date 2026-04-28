19c452bc7212bb7eab16fb20fa3c13cd2c8ca5da
KavinduNirmal <65773645+KavinduNirmal@users.noreply.github.com>
2026-04-28 09:29:33 +0530

    base api url

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -4,6 +4,9 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
+console.log("imported URL:", import.meta.env.VITE_API_URL);
+console.log("API Base URL:", API.defaults.baseURL);
+
 export const getItems = () => API.get("/items");
 export const getItemById = (id) => API.get(`/items/${id}`);
 export const createItem = (itemData) => API.post("/items", itemData);
